e40923ccc48f72a024de8b9ebb4f72eac9e32273
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2024-03-09 13:11:43 +0100

    organize import

--- price-application/src/main/java/com/sternitc/kafka/kafkastreams/articleprice/application/port/in/NewArticlePricesUseCase.java
+++ price-application/src/main/java/com/sternitc/kafka/kafkastreams/articleprice/application/port/in/NewArticlePricesUseCase.java
@@ -1,9 +1,6 @@
 package com.sternitc.kafka.kafkastreams.articleprice.application.port.in;
 
-import com.sternitc.kafka.kafkastreams.articleprice.application.domain.model.ArticlePrice;
-
 import java.util.Collection;
-import java.util.List;
 
 public interface NewArticlePricesUseCase {
 
@@ -11,6 +8,7 @@ public interface NewArticlePricesUseCase {
 
     record NewArticlePrice(String name, Integer price) {
     }
+
     record NewArticlePrices(Collection<NewArticlePrice> newPrices) {
     }
 }

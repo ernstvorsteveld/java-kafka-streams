11cbfe5118f76a786d641ca4ccf56c1414695b80
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2024-03-09 11:06:44 +0100

    remove unused imports

--- src/main/java/com/sternitc/kafka/kafkastreams/articleprice/adapter/out/messaging/NewArticlePricePublisherPortKafka.java
+++ src/main/java/com/sternitc/kafka/kafkastreams/articleprice/adapter/out/messaging/NewArticlePricePublisherPortKafka.java
@@ -3,8 +3,6 @@ package com.sternitc.kafka.kafkastreams.articleprice.adapter.out.messaging;
 import com.sternitc.kafka.kafkastreams.articleprice.application.port.out.messaging.NewArticlePricePublisherPort;
 import org.springframework.kafka.core.KafkaTemplate;
 
-import java.util.logging.Logger;
-
 public class NewArticlePricePublisherPortKafka implements NewArticlePricePublisherPort {
 
     private final KafkaTemplate<String, NewPriceMessage> articlePriceKafkaTemplate;

ee4bc17cce6cda41a8f976583de4054cb9246888
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2024-03-09 13:40:38 +0100

    add testcase for bean loading

--- new-topic-handler/src/main/java/com/sternitc/kafka/kafkastreams/articlepricetopics/ArticlePriceTopicConfiguration.java
+++ new-topic-handler/src/main/java/com/sternitc/kafka/kafkastreams/articlepricetopics/ArticlePriceTopicConfiguration.java
@@ -2,11 +2,13 @@ package com.sternitc.kafka.kafkastreams.articlepricetopics;
 
 import com.sternitc.kafka.kafkastreams.articlepricetopics.application.service.NewTopicService;
 import com.sternitc.kafka.kafkastreams.articlepricetopics.port.in.NewTopicUseCase;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ArticlePriceTopicConfiguration {
 
+    @Bean
     public NewTopicUseCase newTopicUseCase() {
         return new NewTopicService();
     }

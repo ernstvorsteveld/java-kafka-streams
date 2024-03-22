71e2ccddf459e6c3148af2db86a759e0a49d6848
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2024-03-22 14:52:40 +0100

    wip

---
+++ threshold-application/src/main/java/com/sternitc/kafka/thresholdapplication/ThresholdApplication.java
@@ -0,0 +1,12 @@
+package com.sternitc.kafka.thresholdapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThresholdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThresholdApplication.class, args);
+    }
+}

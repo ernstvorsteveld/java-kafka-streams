ab824f80699c1016fd60a9261e0ef460249b207f
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2024-05-27 10:18:11 +0200

    add final

--- boundary-module/src/main/java/com/sternitc/kafka/kafkastreams/boundaryapplication/adapter/in/http/PriceBoundaryApiDelegateImpl.java
+++ boundary-module/src/main/java/com/sternitc/kafka/kafkastreams/boundaryapplication/adapter/in/http/PriceBoundaryApiDelegateImpl.java
@@ -12,7 +12,7 @@ import reactor.core.publisher.Mono;
 @RestController
 public class PriceBoundaryApiDelegateImpl implements PriceBoundaryApplicationApi {
 
-    private NewPriceBoundaryUseCase newPriceBoundaryUseCase;
+    private final NewPriceBoundaryUseCase newPriceBoundaryUseCase;
     private final LocationCreator locationCreator;
     private final Mapper mapper;
 
